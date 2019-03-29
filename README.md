5ed3e5057499a15c3d47b4d05b3836e726a5104b
oberwiesenfeld <oberwiesenfeld@users.noreply.github.com>
2019-03-29 19:07:08 +0100

    checklink script

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# checklink-oberwiesenfeld-tech
+# Check Link Utility [![Build Status](https://travis-ci.org/oberwiesenfeld/checklink-oberwiesenfeld-tech.png)](https://travis-ci.org/oberwiesenfeld/checklink-oberwiesenfeld-tech)
+
+Run a check to find broken links by means of a docker container build from [W3C link-checker](https://github.com/w3c/link-checker).
+The report is written to a sub directory called results. It contains the html report of the test run.
+
+```bash
+./checklink-oberwiesenfeld-tech.sh
+```
+
+In OS-X environments the result file is opened automatically after the rest run.
+
+## Return values
+
+The script `checklink-oberwiesenfeld-tech.sh` returns 
+- 1 : if broken links are present
+- 0 : if no broken links are present
---
+++ checklink-oberwiesenfeld-tech.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#
+# --suppress-broken code "-1" steht für links die durch robots.txt untersagt sind.
+#
+
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+readonly result_timestamp=$(date "+%Y_%m_%d_%H_%M_%S")
+readonly result_dir=${SCRIPT_PATH}/results
+readonly result_file=${result_dir}/results_${result_timestamp}.html
+
+if [[ ! -d ${result_dir} ]]; then
+  mkdir ${result_dir}
+fi  
+
+docker run f800r/checklink:latest \
+  --html \
+  --broken \
+  --suppress-broken -1:https://stats.wp.com/w.js?59 \
+  --suppress-broken -1:https://pixel.wp.com/b.gif?v=noscript \
+  --suppress-broken -1:http://www.aboutads.info/choices/ \
+  --suppress-broken -1:https://youtu.be/FhUUmTEfBkc \
+  --suppress-broken -1:https://github.com/oberwiesenfeld \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2Fimpressum%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2Fimpressum%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2F2018%2F10%2F12%2Fcheckliste-fuer-die-motorradreise%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2F2018%2F10%2F12%2Fcheckliste-fuer-die-motorradreise%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2Fliteraturverzeichnis%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2Fliteraturverzeichnis%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2F2018%2F03%2F09%2Fin-muenchen-am-oberwiesenfeld%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2F2018%2F03%2F09%2Fin-muenchen-am-oberwiesenfeld%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2Fdatenschutz%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2Fdatenschutz%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2F2018%2F06%2F14%2Fkolbenringe%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2F2018%2F06%2F14%2Fkolbenringe%2F&for=wpcom-auto-discovery" \
+  --suppress-broken 405:"https://www.instagram.com/unsupportedbrowser/" \
+  --suppress-broken 405:"http://instagram.com/about/legal/privacy/" \
+  --suppress-broken 403:"https://vimeo.com/privacy" \
+  --suppress-fragment "https://widgets.wp.com/likes/master.html?ver=20190321#ver=20190321&lang=de" \
+  --suppress-fragment "https://books.google.de/books?id=Fd6sBgAAQBAJ&lpg=PA6&hl=de&pg=PP1#v=onepage&q&f=false" \
+  --depth 2 \
+  --exclude ".*https:\/\/www.instagram.com\/unsupportedbrowser\/" \
+  --exclude ".*http:\/\/instagram.com\/about\/legal\/privacy\/" \
+  --exclude ".*https:\/\/public-api.wordpress.com\/oembed.*" \
+  --exclude ".*https:\/\/widgets.wp.com\/likes\/master.html.*" \
+  --exclude ".*http:\/\/instagram.com\/about\/legal\/privacy\/.*" \
+  --exclude ".*https:\/\/r-login.wordpress.com\/remote-login.*" \
+  --exclude-docs ".*https:\/\/public-api.wordpress.com\/oembed.*" \
+  --exclude-docs ".*https:\/\/widgets.wp.com\/likes\/master.html.*" \
+  --exclude-docs ".*http:\/\/instagram.com\/about\/legal\/privacy\/.*" \
+  --exclude-docs ".*https:\/\/r-login.wordpress.com\/remote-login.*" \
+  https://oberwiesenfeld.tech | \
+  tee ${result_file}
+
+readonly broken_links=$(grep -i "broken links" ${result_file} | wc -l|tr -d '[:space:]')
+echo "broken-links=${broken_links}"
+
+if [[ "${OSTYPE}" == *darwin* ]]; then
+  open ${result_file}
+fi
+
+if [[ "${broken_links}" > "0" ]]; then
+  exit 1
+fi
+
+exit 0
