d681b151e7f0cee726e94e1fb49727402496a4ac
oberwiesenfeld <oberwiesenfeld@users.noreply.github.com>
2021-05-15 18:27:49 +0200

    update check urls

--- checklink-oberwiesenfeld-tech.sh
+++ checklink-oberwiesenfeld-tech.sh
@@ -38,11 +38,16 @@ docker run f800r/checklink:latest \
   --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2F2019%2F06%2F12%2Fbmw-r-18%2F&for=wpcom-auto-discovery" \
   --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2F2019%2F11%2F17%2Fkennzeichen%2F&for=wpcom-auto-discovery" \
   --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2F2019%2F11%2F17%2Fkennzeichen%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2F2021%2F04%2F05%2Fm2b15%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2F2021%2F04%2F05%2Fm2b15%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2F2020%2F12%2F28%2Fkennwerte%2F&for=wpcom-auto-discovery" \
+  --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2F2020%2F12%2F28%2Fkennwerte%2F&for=wpcom-auto-discovery" \
   --suppress-broken -1:"https://oberwiesenfeld.tech/2019/06/12/bmw-r-18/?share=facebook" \
   --suppress-broken -1:"https://oberwiesenfeld.tech/2019/11/17/kennzeichen/?share=facebook" \
   --suppress-broken -1:"https://www.youtube.com/results?search_query=%23ABavarianSoulstory" \
   --suppress-broken -1:"https://stats.wp.com/w.js?61" \
   --suppress-broken -1:"http://instagram.com/about/legal/privacy/" \
+  --suppress-broken -1:"https://cdn.ampproject.org/" \
   --suppress-broken 405:"https://www.instagram.com/unsupportedbrowser/" \
   --suppress-broken 405:"https://oberwiesenfeld.tech/xmlrpc.php" \
   --suppress-broken 403:"https://vimeo.com/privacy" \
