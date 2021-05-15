c06d532204e9d2fc1f306bc59614c9502186fe0c
oberwiesenfeld <48629253+oberwiesenfeld@users.noreply.github.com>
2021-05-15 19:39:58 +0200

    blanks after line cont removed

--- checklink-oberwiesenfeld-tech.sh
+++ checklink-oberwiesenfeld-tech.sh
@@ -47,7 +47,7 @@ docker run f800r/checklink:latest \
   --suppress-broken -1:"https://www.youtube.com/results?search_query=%23ABavarianSoulstory" \
   --suppress-broken -1:"https://stats.wp.com/w.js?61" \
   --suppress-broken -1:"http://instagram.com/about/legal/privacy/" \
-  --suppress-broken 403:"https://vimeo.com/privacy" \  
+  --suppress-broken 403:"https://vimeo.com/privacy" \
   --suppress-broken 404:"https://cdn.ampproject.org/" \
   --suppress-broken 405:"https://www.instagram.com/unsupportedbrowser/" \
   --suppress-broken 405:"https://oberwiesenfeld.tech/xmlrpc.php" \
