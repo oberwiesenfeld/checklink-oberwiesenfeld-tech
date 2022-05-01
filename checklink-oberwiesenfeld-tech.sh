674a3e42fbf1fc7135f3a5b71e42d8b49b25505c
oberwiesenfeld <48629253+oberwiesenfeld@users.noreply.github.com>
2022-05-01 12:36:39 +0200

    check link depth=1

--- checklink-oberwiesenfeld-tech.sh
+++ checklink-oberwiesenfeld-tech.sh
@@ -55,7 +55,7 @@ docker run f800r/checklink:latest \
   --suppress-broken 405:"https://oberwiesenfeld.tech/xmlrpc.php" \
   --suppress-fragment "https://widgets.wp.com/likes/master.html?ver=20200826#ver=20200826&lang=de" \
   --suppress-fragment "https://books.google.de/books?id=Fd6sBgAAQBAJ&lpg=PA6&hl=de&pg=PP1#v=onepage&q&f=false" \
-  --depth 2 \
+  --depth 1 \
   --location "https://oberwiesenfeld.tech" \
   --exclude ".*https:\/\/www.instagram.com\/unsupportedbrowser\/" \
   --exclude ".*https:\/\/www.facebook.com\/unsupportedbrowser\/" \
