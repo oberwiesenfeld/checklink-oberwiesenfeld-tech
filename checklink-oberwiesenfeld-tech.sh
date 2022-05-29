6b62f440630ef3a33f9c4d7997c25616d6a48953
oberwiesenfeld <48629253+oberwiesenfeld@users.noreply.github.com>
2022-05-29 17:38:57 +0200

    allow 404 from gstatic.com

--- checklink-oberwiesenfeld-tech.sh
+++ checklink-oberwiesenfeld-tech.sh
@@ -48,6 +48,7 @@ docker run f800r/checklink:latest \
   --suppress-broken -1:"https://www.youtube.com/results?search_query=%23ABavarianSoulstory" \
   --suppress-broken -1:"http://instagram.com/about/legal/privacy/" \
   --suppress-broken -1:"https://subscribe.wordpress.com/" \
+  --suppress-broken -1:"https://fonts.gstatic.com/" \
   --suppress-broken -1:"https://www.deutsche-biographie.de/pnd120252872.html" \
   --suppress-broken -1:"data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9JzQ0MCcgd2lkdGg9JzczOScgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJyB2ZXJzaW9uPScxLjEnLz4=" \
   --suppress-broken -1:"https://apps.apple.com/de/app/erste-hilfe-drk/id404519165" \
