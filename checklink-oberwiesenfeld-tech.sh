8e7b93c31baa8842d3467eed48ca31bb1b070dd2
oberwiesenfeld <oberwiesenfeld@users.noreply.github.com>
2022-09-25 19:27:55 +0200

    reflect new wordpress robots.txt

--- checklink-oberwiesenfeld-tech.sh
+++ checklink-oberwiesenfeld-tech.sh
@@ -21,6 +21,46 @@ docker run f800r/checklink:latest \
   --suppress-broken -1:http://www.aboutads.info/choices/ \
   --suppress-broken -1:https://youtu.be/FhUUmTEfBkc \
   --suppress-broken -1:https://github.com/oberwiesenfeld \
+  --suppress-broken -1:https://wordpress.com/read/feeds/81514772 \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2021%252F04%252F05%252Fm2b15%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2018%252F03%252F09%252Fin-muenchen-am-oberwiesenfeld%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F04%252F22%252Freitwagen%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2018%252F03%252F09%252Fin-muenchen-am-oberwiesenfeld%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252Fimpressum%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252Fdatenschutz%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252Fliteraturverzeichnis%252F \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/4 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/315 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/281 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/504 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/53 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/4 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/20 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/544 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/315 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/623 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/443 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/17 \
+  --suppress-broken -1:https://wordpress.com/read/blogs/143830998/posts/602 \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2018%252F10%252F12%252Fcheckliste-fuer-die-motorradreise%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F11%252F17%252Fkennzeichen%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F06%252F12%252Fbmw-r-18%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2018%252F06%252F14%252Fkolbenringe%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2018%252F06%252F14%252Fkolbenringe%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F11%252F17%252Fkennzeichen%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F06%252F12%252Fbmw-r-18%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2018%252F06%252F14%252Fkolbenringe%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F06%252F12%252Fbmw-r-18%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2020%252F12%252F28%252Fkennwerte%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F06%252F12%252Fbmw-r-18%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F06%252F12%252Fbmw-r-18%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F06%252F12%252Fbmw-r-18%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F11%252F17%252Fkennzeichen%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2020%252F12%252F28%252Fkennwerte%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F11%252F17%252Fkennzeichen%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2019%252F06%252F12%252Fbmw-r-18%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2018%252F06%252F14%252Fkolbenringe%252F \
+  --suppress-broken -1:https://wordpress.com/log-in?redirect_to=https%3A%2F%2Fr-login.wordpress.com%2Fremote-login.php%3Faction%3Dlink%26back%3Dhttps%253A%252F%252Foberwiesenfeld.tech%252F2020%252F12%252F28%252Fkennwerte%252F \
   --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2Fimpressum%2F&for=wpcom-auto-discovery" \
   --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=xml&url=https%3A%2F%2Foberwiesenfeld.tech%2Fimpressum%2F&for=wpcom-auto-discovery" \
   --suppress-broken -1:"https://public-api.wordpress.com/oembed/?format=json&url=https%3A%2F%2Foberwiesenfeld.tech%2F2018%2F10%2F12%2Fcheckliste-fuer-die-motorradreise%2F&for=wpcom-auto-discovery" \
@@ -52,6 +92,7 @@ docker run f800r/checklink:latest \
   --suppress-broken -1:"data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9JzQ0MCcgd2lkdGg9JzczOScgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJyB2ZXJzaW9uPScxLjEnLz4=" \
   --suppress-broken -1:"https://apps.apple.com/de/app/erste-hilfe-drk/id404519165" \
   --suppress-broken -1:"https://www.linkedin.com/in/gudrun-muschalla-40b3967/" \
+  --suppress-broken -1:"https://wordpress.com/log-in" \
   --suppress-broken 403:"https://vimeo.com/privacy" \
   --suppress-broken 403:"https://yokohamahotrodcustomshow.com/hcs2018-report/show_hcs2018/" \
   --suppress-broken 404:"https://cdn.ampproject.org/" \
